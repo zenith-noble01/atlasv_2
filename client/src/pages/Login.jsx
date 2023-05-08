9ed5a42b0c5908b03225ba8d6a9e610d7694c18e
zenith-noble01 <webdev686@gmail.com>
2023-05-09 00:47:45 +0100

    made some changes in the files

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "../styles/auth.scss";
+import { BiPlus } from "react-icons/bi";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 
 const Login = () => {
   const [userData, setUserData] = useState({
@@ -7,6 +9,8 @@ const Login = () => {
     email: "",
     password: "",
   });
+
+  const [isvissible, setIsVissible] = useState(false);
 
   const { username, email, password } = userData;
 
@@ -24,42 +28,46 @@ const Login = () => {
   return (
     <div className="app__auth login">
       <div className="auth__container">
-        <div className="auth__left">
-          <div className="left__header">
-            <div className="logo">atlas.</div>
+        <div className="auth__header">
+          <h1>Welcome back</h1>
+          <p>Join Cameroon Largest Learning community.</p>
+        </div>
+        {/*    <div className="recent__container">
+          <div className="recent__header">
+            <p>Recent Logins</p>
+          </div>
+          <div className="add__container">
+            <div className="nouser">
+              <div className="plus_circle">
+                <BiPlus />
+              </div>
+              <p>Add account</p>
+            </div>
           </div>
         </div>
-        <div className="auth__right">
-          <div className="right__header">
-            <p>Login</p>
-          </div>
-          <form onSubmit={handleSubmit} className="auth__form">
-            <div className="input__container">
-              <input
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={username}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="input__container">
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                value={email}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="input__container">
-              <input
-                type=""
-                placeholder="Create a password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-              />
+        <div className="or__container">
+          <p>or</p>
+        </div> */}
+        <div className="login__form">
+          <form className="auth__form" onSubmit={handleSubmit}>
+            <div className="inputs">
+              <div className="input__container">
+                <input
+                  type="text"
+                  placeholder="Phone number, username or email"
+                />
+              </div>
+              <div className="input__container">
+                <input
+                  type={isvissible ? "text" : "password"}
+                  placeholder="Password"
+                />
+                {isvissible ? (
+                  <BsEyeFill onClick={() => setIsVissible(!isvissible)} />
+                ) : (
+                  <BsEyeSlashFill onClick={() => setIsVissible(!isvissible)} />
+                )}
+              </div>
             </div>
           </form>
         </div>
---
+++ client/src/utils/commented.js
@@ -0,0 +1,41 @@
+{/*  <div className="auth__container">
+        <div className="auth__left">
+          <div className="left__header">
+            <div className="logo">atlas.</div>
+          </div>
+        </div>
+        <div className="auth__right">
+          <div className="right__header">
+            <p>Login</p>
+          </div>
+          <form onSubmit={handleSubmit} className="auth__form">
+            <div className="input__container">
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="input__container">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="input__container">
+              <input
+                type=""
+                placeholder="Create a password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </div>
+          </form>
+        </div>
+      </div> */}
